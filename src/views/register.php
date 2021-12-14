242b5902a47c665206d1b9cdb7d233af9b71bd3e
LazareC <lazarela17@gmail.com>
2021-12-14 16:19:05 +0100

    update register view

--- src/views/register.php
+++ src/views/register.php
@@ -19,19 +19,19 @@
             <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">S'enregistrer</h1>
             <div>
                 <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Identifiant</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Identifiant" value="<?php echo $username; ?>" />
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="Identifiant" required/>
             </div>
             <div>
                 <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="exemple@email.fr" value="<?php echo $email; ?>" />
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="email" name="email" id="email" placeholder="exemple@email.fr" required/>
             </div>
             <div>
                 <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Mot de passe</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Mot de passe" />
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="password" name="password" id="password" placeholder="Mot de passe" required/>
             </div>
             <div>
-                <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirmation du mot de passe</label>
-                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="Confirmation du mot de passe" />
+             <!--   <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirmation du mot de passe</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="Confirmation du mot de passe" />-->
             </div>
             <button type="submit" name="reg_user" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">S'enregistrer</button>
         </form>
